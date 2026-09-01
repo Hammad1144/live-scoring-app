e8fc3690e5811c1315d6186a0d43657297846b9d
Hammad Anwar <73832440+Hammad1144@users.noreply.github.com>
2026-09-02 03:48:36 +0500

    Route small screens to mobile Viewer PWA

--- App.web.tsx
+++ App.web.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useWindowDimensions } from 'react-native';
+import { ViewerMobileApp } from './src/web/ViewerMobileApp';
 import { ViewerWebApp } from './src/web/ViewerWebApp';
 
 export default function App() {
-  return <ViewerWebApp />;
+  const { width } = useWindowDimensions();
+  return width < 620 ? <ViewerMobileApp /> : <ViewerWebApp />;
 }
