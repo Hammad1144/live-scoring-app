a13191bd6cec0c911f0f0c60b3386158ba4a1da6
Hammad Anwar <73832440+Hammad1144@users.noreply.github.com>
2026-09-02 00:23:10 +0500

    Add Viewer PWA service worker

---
+++ web/sw.js
@@ -0,0 +1,29 @@
+const CACHE = 'cricket-zone-viewer-v1';
+const SHELL = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './icon.png'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  const url = new URL(event.request.url);
+  if (event.request.method !== 'GET' || url.origin !== self.location.origin) return;
+
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      const network = fetch(event.request).then(response => {
+        if (response.ok) caches.open(CACHE).then(cache => cache.put(event.request, response.clone()));
+        return response;
+      }).catch(() => cached);
+      return cached || network;
+    })
+  );
+});
